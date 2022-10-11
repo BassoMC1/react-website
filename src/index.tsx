1149b76f7bd25e5d49661ffc9ab8ff0d8c8386e6
BassoMC <Bassow05@gmail.com>
2022-10-12 01:08:32 +0200

    added Routes :)

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { render } from 'react-dom';
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Apps from './Pages/App';
+import Test from './Pages/Test';
+import Error404 from './Pages/404';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  render() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Apps />} ></Route>
+        <Route path="/Test" element={<Test />}></Route>
+        <Route path="*" element={<Error404 />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+}
+render(<App />, document.getElementById('root'));
